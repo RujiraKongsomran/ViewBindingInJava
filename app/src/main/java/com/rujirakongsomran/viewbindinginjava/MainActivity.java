8a1e5e30a570b63e0e07d8c4a42b9cfc4506b8d5
RujiraKongsomran <rujirabiwberry@gmail.com>
2020-08-16 20:40:46 +0700

    Write JAVA Code and add id in TextView

--- app/src/main/java/com/rujirakongsomran/viewbindinginjava/MainActivity.java
+++ app/src/main/java/com/rujirakongsomran/viewbindinginjava/MainActivity.java
@@ -3,12 +3,24 @@ package com.rujirakongsomran.viewbindinginjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.rujirakongsomran.viewbindinginjava.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        //setContentView(R.layout.activity_main);
+
+        binding.tvResult.setText("View Binding");
     }
 }
